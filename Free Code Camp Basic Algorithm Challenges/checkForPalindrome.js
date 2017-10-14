2b829ac988d3a71325e427b506e03ff9eb8c095d
Peter Lewicki <bryonylewicki@Bryonys-iMac.local>
2017-10-14 16:47:40 -0400

    FFC algorithms

---
+++ Free Code Camp Basic Algorithm Challenges/checkForPalindrome.js
@@ -0,0 +1,21 @@
+/* Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.  */
+
+
+function palindrome(str) {
+  str = str.toLowerCase().replace(/[\W_]/g, '');
+  for(var i = 0, len = str.length - 1; i < len/2; i++) {
+    if(str[i] !== str[len-i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+palindrome("eye");
